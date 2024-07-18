876d4f16e2dd88f900375686e5e76826be3e5483
leoMa23 <leonelfranco09@gmail.com>
2024-07-18 17:47:38 -0300

    Agregando la estructura HTML

---
+++ app.js
@@ -0,0 +1,20 @@
+// Las llaves de encriptación que utilizaremos
+// son las siguientes:
+// la letra "a" es convertida para "ai"
+// la letra "e" es convertida para "enter"
+// la letra "i" es convertida para "imes"
+// la letra "o" es convertida para "ober"
+// la letra "u" es convertida para "ufat"
+
+
+function encriptar(){
+
+}
+
+function desencriptar(){
+
+}
+
+function copiar(){
+
+}
