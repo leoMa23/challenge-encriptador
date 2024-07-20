618bdb8fb24bdcbe575cd7c0e7f473d66899d3a5
leoMa23 <leonelfranco09@gmail.com>
2024-07-20 02:05:10 -0300

    se agrega el CSS

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-// Las llaves de encriptación que utilizaremos
-// son las siguientes:
-// la letra "a" es convertida para "ai"
-// la letra "e" es convertida para "enter"
-// la letra "i" es convertida para "imes"
-// la letra "o" es convertida para "ober"
-// la letra "u" es convertida para "ufat"
-
 
 function encriptar(){
-
+    let texto = document.getElementById("texto").value;
+    let parrafo = document.getElementById("mensaje");
+    let imagen = document.getElementById("imagen");
+    let textoEncriptado = texto.replace(/i/gi, "ines")
+                                .replace(/a/gi, "ai")
+                                .replace(/e/gi, "enter")
+                                .replace(/o/gi, "ober")
+                                .replace(/u/gi, "ufat");
+                                
+    if(texto.length !=0){
+        imagen.style.display= "none";
+        texto = textoEncriptado;
+        parrafo.textContent = texto;
+    }else{
+        imagen.style.display="none";
+        parrafo.textContent = "Debes ingresar algun texto para encriptar"; 
+    }
 }
 
 function desencriptar(){
-
+    let texto = document.getElementById("texto").value;
+    let parrafo = document.getElementById("mensaje");
+    let imagen = document.getElementById("imagen");
+    let textoEncriptado = texto.replace(/ai/gi, "a")
+                                .replace(/enter/gi, "e")
+                                .replace(/ines/gi, "i")
+                                .replace(/ober/gi, "o")
+                                .replace(/ufat/gi, "u");
+    if(texto.length !=0){
+        imagen.style.display = "none";
+        texto = textoEncriptado;
+        parrafo.textContent = texto;
+    }else{
+        imagen.style.display= "none";
+        parrafo.textContent= "Debes ingresar algun texto para desencriptar"
+    }
 }
 
 function copiar(){
-
+    let copia = document.getElementById("mensaje");
+    if(copia){
+        copia.select();
+        document.execCommand("copy");
+    }
 }
